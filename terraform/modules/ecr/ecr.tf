3702950255e370e787e0bd87416e309360470e52
gewa11281 <desbaina@yahoo.com>
2026-04-26 01:06:23 -0700

    fix: remove invalid comment from force_delete line

--- terraform/modules/ecr/ecr.tf
+++ terraform/modules/ecr/ecr.tf
@@ -7,7 +7,7 @@
 resource "aws_ecr_repository" "app" {
   name                 = var.repository_name
   image_tag_mutability = "MUTABLE"
-  force_delete         = true    ← add this line
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
